13f8847c72254f3103fa519b3a4a404c9f5588c4
Mohanad Affify <66140534+mohanadaffify@users.noreply.github.com>
2021-08-21 11:43:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+#  Create Data warehouse using Amazon Redshift
+-------------------------
+
+### Introduction
+
+A music streaming startup, Sparkify, has grown their user base and song database and want to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+As their data engineer, I am tasked with building an ETL pipeline that extracts their data from S3, stages them in Redshift, and transforms data into a set of dimensional tables for their analytics team to continue finding insights in what songs their users are listening to. 
+
+### Datasets
+Datasets used in this project are provided in two public **S3 buckets**. One bucket contains info about songs and artists, the second bucket has info concerning actions done by users (which song are listening, etc.. ). The objects contained in both buckets 
+are *JSON* files. 
+
+The **Redshift** service is where data will be ingested and transformed, in fact though `COPY` command we will access to the JSON files inside the buckets and copy their content on our *staging tables*.
+
+### Database Schema
+We have two staging tables which *copy* the JSON file inside the  **S3 buckets**.
+#### Staging Table 
++ **staging_songs** - info about songs and artists
++ **staging_events** - actions done by users 
+
+
+I createa a star schema optimized for queries on song play analysis. This includes the following tables:
+
+#### Fact Table 
++ **songplays** - records in event data associated with song plays i.e. records with page NextSong
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+#### Dimension Tables
++ **users** - users in the app
++ **songs** - songs in music database
++ **artists** - artists in music database
++ **time** - timestamps of records in **songplays** broken down into specific units
+
+The database schema is shown in the figure below 
+![schema](./images/db_schema.PNG)
+
+### Data Warehouse Configurations and Setup
+* Create an `IAM Role` that makes `Redshift` able to access `S3 bucket` (ReadOnly)
+* Create a `security group` to authorize access to Redshift through `EC2` instance 
+* Create a `RedShift Cluster` and get the `DWH_ENDPOIN(Host address)` and `DWH_ROLE_ARN` and fill the config file.
+* fill the `dwh.cfg` with redshift database user and password. 
+
+### ETL Pipeline
++ Created staging tables to store the data from `S3 buckets`.
++ copied the data from `S3 buckets` to the staging tables in the `Redshift Cluster`.
++ loaded the data from the staging tables into the `Redshift cluster`. 
+
+### Project Structure
+
++ `create_tables.py` - created the fact and dimension tables for the star schema in `RedShift`
++ `etl.py` - This script load data from S3 into staging tables on Redshift and then process that data into the analytics tables on Redshift.
++ `sql_queries.py` - This file contains variables with SQL statements , which will be imported in `create_tables.py` and `etl.py`.
++ `dhw.cfg` - Configuration file used that contains info about `Redshift`, `IAM` and `S3`.
+
+### How to Run
+
+1. Create tables by running `create_tables.py`.
+
+2. Execute ETL process by running `etl.py`.
+
+
+
+
+
+
+
---
+++ sql_queries (1).py
@@ -0,0 +1,204 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+staging_events_table_create= ("""
+CREATE TABLE staging_events(
+    event_id INT IDENTITY(0,1),
+    artist_name VARCHAR,
+    auth VARCHAR,
+    user_first_name VARCHAR,
+    user_gender  VARCHAR,
+    item_in_session	INTEGER,
+    user_last_name VARCHAR,
+    song_length	DOUBLE PRECISION, 
+    user_level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration VARCHAR,
+    session_id BIGINT,
+    song_title VARCHAR,
+    status INTEGER, 
+    ts VARCHAR,
+    user_agent TEXT,
+    user_id VARCHAR,
+    PRIMARY KEY (event_id))
+""")
+
+staging_songs_table_create = ("""
+CREATE TABLE staging_songs(
+    song_id VARCHAR(100),
+    num_songs INTEGER,
+    artist_id VARCHAR,
+    artist_latitude DOUBLE PRECISION,
+    artist_longitude DOUBLE PRECISION,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    title VARCHAR,
+    duration DOUBLE PRECISION,
+    year INTEGER,
+    PRIMARY KEY (song_id))
+""")
+
+songplay_table_create = ("""
+CREATE TABLE songplays(
+    songplay_id INT IDENTITY(0,1) NOT NULL,
+    start_time TIMESTAMP,
+    user_id VARCHAR NOT NULL,
+    level VARCHAR,
+    song_id VARCHAR NOT NULL,
+    artist_id VARCHAR NOT NULL,
+    session_id BIGINT NOT NULL,
+    location VARCHAR,
+    user_agent TEXT,
+    PRIMARY KEY (songplay_id))
+""")
+
+user_table_create = ("""
+CREATE TABLE users(
+    user_id VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR ,
+    gender VARCHAR,
+    level VARCHAR,
+    PRIMARY KEY (user_id))
+""")
+
+song_table_create = ("""
+CREATE TABLE songs(
+    song_id VARCHAR,
+    title VARCHAR,
+    artist_id VARCHAR NOT NULL,
+    year INTEGER,
+    duration DOUBLE PRECISION,
+    PRIMARY KEY (song_id))
+""")
+
+artist_table_create = ("""
+CREATE TABLE artists(
+    artist_id VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    location VARCHAR,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    PRIMARY KEY (artist_id))
+""")
+
+time_table_create = ("""
+CREATE TABLE time(
+    start_time TIMESTAMP NOT NULL,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
+    PRIMARY KEY (start_time))
+""")
+# STAGING TABLES
+
+staging_events_copy = ("""copy staging_events 
+                          from {}
+                          iam_role {}
+                          json {};
+                       """).format(config.get('S3','LOG_DATA'), config.get('IAM_ROLE', 'ARN'), config.get('S3','LOG_JSONPATH'))
+
+staging_songs_copy = ("""copy staging_songs 
+                          from {} 
+                          iam_role {}
+                          json 'auto';
+                      """).format(config.get('S3','SONG_DATA'), config.get('IAM_ROLE', 'ARN'))
+
+# FINAL TABLES
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+SELECT  
+    TIMESTAMP 'epoch' + e.ts/1000 * interval '1 second' as start_time, 
+    e.user_id, 
+    e.user_level, 
+    s.song_id,
+    s.artist_id, 
+    e.session_id,
+    e.location, 
+    e.user_agent
+FROM staging_events e, staging_songs s
+WHERE e.page = 'NextSong' 
+AND e.song_title = s.title 
+AND e.artist_name = s.artist_name 
+AND e.song_length = s.duration
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT  
+    user_id, 
+    user_first_name, 
+    user_last_name, 
+    user_gender, 
+    user_level
+FROM staging_events
+WHERE page = 'NextSong'
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration) 
+SELECT DISTINCT 
+    song_id, 
+    title,
+    artist_id,
+    year,
+    duration
+FROM staging_songs
+WHERE song_id IS NOT NULL
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+SELECT DISTINCT 
+    artist_id,
+    artist_name,
+    artist_location,
+    artist_latitude,
+    artist_longitude
+FROM staging_songs
+WHERE artist_id IS NOT NULL
+""")
+
+time_table_insert = ("""
+INSERT INTO time(start_time, hour, day, week, month, year, weekDay)
+SELECT 
+    TIMESTAMP 'epoch' + e.ts/1000 * interval '1 second' as start_time, 
+    extract(hour from start_time),
+    extract(day from start_time),
+    extract(week from start_time), 
+    extract(month from start_time),
+    extract(year from start_time), 
+    extract(dayofweek from start_time)
+FROM staging_events
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
